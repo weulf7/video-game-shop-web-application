1385b78f38f5f3850351b4e1930a766c6f0ad489
Antal Matyas <antalmatyas9@gmail.com>
2020-11-06 13:32:26 +0200

    added method add product to cart and also binded it to an event

--- video-game-shop-web/js/shop.js
+++ video-game-shop-web/js/shop.js
@@ -12,6 +12,25 @@ window.Shop = {
 
     },
 
+    addProductToCart: function (productId) {
+        const userId = 5;
+
+        const requestBody = {
+            userId: userId,
+            productId: productId
+        }
+
+        $.ajax({
+            url: Shop.API_URL + "/carts",
+            contentType: "application/json",
+            method: "PUT",
+            data: JSON.stringify(requestBody)
+        }).done(function () {
+            window.location.replace("cart.html");
+        })
+
+    },
+
     getProductHtml: function (product) {
         return `
         <div class="col-md-3 col-sm-6">
@@ -19,7 +38,7 @@ window.Shop = {
                         <div class="product-upper">
                             <img src="img/product-2.jpg" alt="">
                         </div>
-                        <h2><a href="">${product.name}{</a></h2>
+                        <h2><a href="">${product.name}</a></h2>
                         <div class="product-carousel-price">
                             <ins>$${product.price}</ins> <del>$999.00</del>
                         </div>  
@@ -34,14 +53,28 @@ window.Shop = {
 
     },
 
-    displayProduct:function (products){
-        let productsHtml='';
+    displayProduct: function (products) {
+        let productsHtml = '';
 
-        products.forEach(product  => productsHtml+=Shop.getProductHtml(product));
+        products.forEach(product => productsHtml += Shop.getProductHtml(product));
 
         $('.single-product-area .row:first-child').html(productsHtml);
+
+    },
+
+    bindEvents: function () {
+
+        $('.single-product-area .row:first-child').delegate('.add_to_cart_button', 'click', function (event) {
+            event.preventDefault();
+
+            let productId = $(this).data('product_id');
+
+            Shop.addProductToCart(productId);
+
+        })
 
     }
 
 };
 Shop.getProducts()
+Shop.bindEvents()
